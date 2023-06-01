731d16f1c2ffe41d4e4e7123fb4a00d956fc34af
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-06-01 23:59:44 +0400

    Initial Commit

---
+++ FlowChat/FlowChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowChatApp.swift
+//  FlowChat
+//
+//  Created by aplle on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
