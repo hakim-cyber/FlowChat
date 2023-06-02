4fffb32dee74410f6631ccedf9a18defde40acdc
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-06-02 16:39:19 +0400

    new

--- FlowChat/Auth views/UserInformationView.swift
+++ FlowChat/Auth views/UserInformationView.swift
@@ -6,51 +6,60 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct UserInformationView: View {
     @State private var userName = ""
     @State private var image:UIImage?
     @State private var showPicker = false
+    
+    @State private var InfoAdded = false
     var body: some View {
-        VStack(spacing: 40){
-            Spacer()
-            TextField("Username", text: $userName)
-                .padding()
-                .background(.thinMaterial)
-                .foregroundColor( .white)
-                .cornerRadius(30)
-                .shadow(color:.gray,radius: 5)
-                .padding(.horizontal,20)
-                .padding(.vertical,2)
-            
-            Image(uiImage: image ?? UIImage())
-                .resizable()
-                .scaledToFit()
-                .background(.black)
-                .clipShape(Circle())
-                .frame(width: 100)
-                .onTapGesture {
-                    showPicker.toggle()
+        ZStack{
+            if InfoAdded {
+                ContentView()
+            }else{
+                VStack(spacing: 40){
+                    Spacer()
+                    TextField("Username", text: $userName)
+                        .padding()
+                        .background(.thinMaterial)
+                        .foregroundColor( .white)
+                        .cornerRadius(30)
+                        .shadow(color:.gray,radius: 5)
+                        .padding(.horizontal,20)
+                        .padding(.vertical,2)
+                    
+                    Image(uiImage: image ?? UIImage())
+                        .resizable()
+                        .scaledToFit()
+                        .background(.black)
+                        .clipShape(Circle())
+                        .frame(width: 100)
+                        .onTapGesture {
+                            showPicker.toggle()
+                        }
+                    Spacer()
+                    Button{
+                        // add user info to firestore
+                    }label: {
+                        Text("Next")
+                            .bold()
+                            .foregroundColor(.white)
+                            .frame(width: 300,height: 60)
+                            .background(
+                                RoundedRectangle(cornerRadius: 40,style: .continuous)
+                                    .fill(Color.indigo)
+                                    .shadow(radius: 5)
+                            )
+                            .padding()
+                    }
+                    .disabled(userName == "" || image == nil)
                 }
-            Spacer()
-            Button{
-              // add user info to firestore
-            }label: {
-                Text("Next")
-                    .bold()
-                    .foregroundColor(.white)
-                    .frame(width: 300,height: 60)
-                    .background(
-                        RoundedRectangle(cornerRadius: 40,style: .continuous)
-                            .fill(Color.indigo)
-                            .shadow(radius: 5)
-                    )
-                    .padding()
+                .sheet(isPresented: $showPicker){
+                    ImagePicker(image: $image)
+                }
             }
-            .disabled(userName == "" || image == nil)
-        }
-        .sheet(isPresented: $showPicker){
-            ImagePicker(image: $image)
         }
     }
 }
